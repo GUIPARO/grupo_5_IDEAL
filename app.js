28fd2a16ea98f67f3b98705953df9171d75cdc35
GUIPARO <guisela2728@gmail.com>
2021-10-26 16:58:58 -0500

    Se resuelven conflictos y cambia tamaño de letra y estilos de botones en la pagina del producto y el carrito de compra

--- app.js
+++ app.js
@@ -21,5 +21,16 @@ app.get('/cotizacion',(req,res) =>{
     res.sendFile(path.resolve(__dirname,"./src/views/cotizacion.html"))
 })
 
+app.get('/product',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,"./src/views/product.html"))
+})
+
+app.get('/productCart',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,"./src/views/productCart.html"))
+})
+
+
+
+
 app.listen(port,()=>{ console.log('El server esta corriendo en el puerto 3000')
 })
