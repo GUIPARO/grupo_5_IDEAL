1c00b35becb741097f44d7fa114dadf2df6206e6
juliancst <jufecas14@gmail.com>
2022-03-03 18:09:51 -0500

    se corrigen conflictos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,27 +1,26 @@
-require("dotenv").config();
-
+require('dotenv').config()
 module.exports = {
-  development: {
-    username: "root",
-    password: "",
-    database: "datos_ideal",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port:"3307",
+  
+    "development": {
+      "username": process.env.DB_USER_DEV,
+      "password": process.env.DB_PASSWORD_DEV,
+      "database": process.env.DB_DATABASE_DEV,
+      "host": process.env.DB_HOST_DEV,
+      "dialect": process.env.DB_DIALECT,
+      "port":process.env.DB_PORT
     },
-  test: {
-    username: "root",
-    password: "null",
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    "test": {
+      "username": "root",
+      "password": "null",
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
     },
-  production: {
-    username: "root",
-    password: "null",
-    database: "database_production",
-    host: " 127.0.0.1",
-    dialect: "mysql",
+    "production": {
+      "username": process.env.DB_USER_PROD,
+      "password": process.env.DB_PASSWORD_PROD,
+      "database": process.env.DB_DATABASE_PROD,
+      "host": process.env.DB_HOST_PROD,
+      "dialect": process.env.DB_DIALECT
     }
   }
-
